ec882fbb35724a2e99acee15a77843c622df262f
sara-k03 <sarayu.kondaveeti@gmail.com>
2025-02-06 00:28:23 -0500

    .npy and .csv files can both be converted to STL files now

--- npyToSTLScript.py
+++ npyToSTLScript.py
@@ -1,7 +1,17 @@
 from npy_to_stl import *
+from csv_to_stl import *
 
-# Enter a the directory containing your NUMPY files 
+
+file_type = input("Are the files csv or npy? ").lower()
+while not(file_type == "csv") and not(file_type == "npy"):
+    file_type = input("Enter file type again: ")
+
 inputPath = input("Enter input path here: ")  # Example: /Users/sarayukondaveeti/NetworkModels
 diameter = float(input("Enter beam diameter in millimeters: ")) # Example: 2.8
 side = float(input("Enter matrix side length in millimeters: ")) # Example: 80
-npy_to_stl(inputPath, diameter, side)
+
+
+if (file_type == "csv"):
+    csv_to_stl(inputPath, diameter, side)
+elif (file_type == "npy"):
+    npy_to_stl(inputPath, diameter, side)
